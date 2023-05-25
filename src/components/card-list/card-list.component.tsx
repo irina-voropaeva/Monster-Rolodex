3ec312069f5ca728469f3e6f355de2ad88dc5af0
Irina Voropayeva <irina.voropayeva@dataart.com>
2023-05-25 17:06:28 +0300

    moved to typescript

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,8 +1,14 @@
-import Card from '../card/card.component'
-
+import Card from '../card/card.component.tsx';
+import { Monster } from '../../App';
 import './card-list.styles.css'
 
-const CardList = ({ monsters }) => (
+
+type CardListProps = {
+    monsters: Monster[];
+}
+
+
+const CardList = ({ monsters }: CardListProps) => (
         <div className='card-list'>
             {monsters.map((monster) => {
             return (
--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,6 +1,10 @@
 import './card.styles.css'
+import { Monster } from '../../App';
 
-const Card = ({ monster }) => (
+interface CardProps {
+  monster: Monster;
+}
+const Card = ({ monster } : CardProps ) => (
   <div className='card-container' key={monster.id}>
     <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2`}/>
     <h2 key={monster.id}>{monster.name}</h2>
